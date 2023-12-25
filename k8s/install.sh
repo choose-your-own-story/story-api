ea14e7cd2fa4f68ef3137a72cbdd36de3b1f2c22
jota <jrojaspin@gmail.com>
2023-12-25 10:35:03 -0300

    asdf 10

--- k8s/install.sh
+++ k8s/install.sh
@@ -1,1 +1,12 @@
-kubectl apply -f k8s.yml
+sed -e "s/\$API_SECRET/$API_SECRET/g" k8s.yml | \
+sed -e "s/\$DOCKER_IMAGE_VERSION/$DOCKER_IMAGE_VERSION/g" | \
+sed -e "s/\$PUBLIC_PATH/$PUBLIC_PATH/g" | \
+sed -e "s/\$POSTGRES_USER/$POSTGRES_USER/g" | \
+sed -e "s/\$POSTGRES_PASSWORD/$POSTGRES_PASSWORD/g" | \
+sed -e "s/\$POSTGRES_DB/$POSTGRES_DB/g" | \
+sed -e "s/\$POSTGRES_HOST/$POSTGRES_HOST/g" | \
+sed -e "s/\$POSTGRES_PORT/$POSTGRES_PORT/g" | \
+sed -e "s/\$DO_SPACES_ENDPOINT/$DO_SPACES_ENDPOINT/g" | \
+sed -e "s/\$DO_SPACES_KEY/$DO_SPACES_KEY/g" | \
+sed -e "s/\$DO_SPACES_SECRET/$DO_SPACES_SECRET/g" | \
+sed -e "s/\$DO_SPACES_NAME/$DO_SPACES_NAME/g" | kubectl apply -f -
